85b6ce2311d3b9c538a324127b8ef054c07ae739
elitan <johan@eliasson.me>
2020-11-12 14:46:56 +0100

    styling and updates on quick start

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -17,8 +17,8 @@ const menuQuickStart = [
     name: "Todos Table",
   },
   {
-    link: "/quick-start/client-app",
-    name: "Client App",
+    link: "/quick-start/frontend-web-app",
+    name: "Frontend Web App",
   },
   {
     link: "/quick-start/authentication",
@@ -196,32 +196,88 @@ export function Menu(props) {
       <MenuHeader>Quick Start</MenuHeader>
       <MenuList menu={menuQuickStart} router={router} />
 
-      <MenuHeader>PostgreSQL</MenuHeader>
+      <MenuHeader>
+        <div className="w-5 h-5 mr-3">
+          <img src="/images/postgresql.svg" />
+        </div>
+        <div>PostgreSQL</div>
+      </MenuHeader>
       <MenuList menu={menuPostgres} router={router} />
 
-      <MenuHeader>Hasura</MenuHeader>
+      <MenuHeader>
+        <div className="w-5 h-5 mr-2">
+          <img src="/images/hasura.svg" />
+        </div>
+        <div>Hasura</div>
+      </MenuHeader>
       <MenuList menu={menuHasura} router={router} />
 
-      <MenuHeader>GraphQL</MenuHeader>
+      <MenuHeader>
+        <div className="w-5 h-5 mr-2">
+          <img src="/images/graphql.svg" />
+        </div>
+        <div>GraphQL</div>
+      </MenuHeader>
       <MenuList menu={menuGraphql} router={router} />
 
-      <MenuHeader>Auth</MenuHeader>
+      <MenuHeader>
+        <svg
+          className="w-5 h-5 mr-2"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+        </svg>
+        <div>Auth</div>
+      </MenuHeader>
       <MenuList menu={menuAuth} router={router} />
 
-      <MenuHeader>Libraries</MenuHeader>
+      <MenuHeader>
+        <svg
+          className="w-5 h-5 mr-2"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+        </svg>
+        <div>Storage</div>
+      </MenuHeader>
+      <MenuList menu={menuStorage} router={router} />
+
+      <MenuHeader>
+        <svg
+          className="w-5 h-5 mr-2"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <div>Libraries</div>
+      </MenuHeader>
       <MenuList menu={menuLibraries} router={router} />
     </div>
   );
 }
 
 function MenuHeader({ children }) {
-  return <div className="text-sm uppercase text-gray-900 pb-1">{children}</div>;
+  return (
+    <div className="flex items-center text-sm uppercase text-gray-900 pb-1">
+      {children}
+    </div>
+  );
 }
 
 function MenuList({ menu }) {
   const router = useRouter();
   return (
-    <div className="mb-6">
+    <div className="pb-4 border-b mb-4">
       {menu.map((item) => {
         const active = item.link === router.pathname;
 

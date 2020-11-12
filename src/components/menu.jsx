49e64d12c735c85989d0667c996602e76e35534c
elitan <johan@eliasson.me>
2020-11-12 16:55:24 +0100

    svg for quick start menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -193,7 +193,22 @@ export function Menu(props) {
 
   return (
     <div>
-      <MenuHeader>Quick Start</MenuHeader>
+      <MenuHeader>
+        <svg
+          className="w-5 h-5 mr-2"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+            clipRule="evenodd"
+          />
+        </svg>
+
+        <div>Quick Start</div>
+      </MenuHeader>
       <MenuList menu={menuQuickStart} router={router} />
 
       <MenuHeader>

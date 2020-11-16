9c7388f0f4d544516928c5d82224886b7eb89e85
Nuno Pato <nunopato@gmail.com>
2020-11-16 11:55:30 -0100

    Fix CLI SVG icon

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -171,9 +171,9 @@ const menuLibraries = [
 const menuCli = [
   {
     link: "/cli",
-    name: "Usage"
-  }
-]
+    name: "Usage",
+  },
+];
 
 // const menu_example_apps = [
 //   {
@@ -291,16 +291,18 @@ export function Menu(props) {
 
       <MenuHeader>
         <svg
-          className="w-5 h-5 mr-2"
-          fill="currentColor"
-          viewBox="0 0 20 20"
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fillRule="evenodd"
-            d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+          ></path>
         </svg>
         <div>CLI</div>
       </MenuHeader>

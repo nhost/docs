27044fc1710cc430387c826845bd10401c17dae9
elitan <johan@eliasson.me>
2020-11-25 11:15:27 +0100

    added smartlook

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,15 +1,28 @@
+import smartlookClient from "smartlook-client";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
+  componentDidMount() {
+    smartlookClient.init("8046e53cf51c2e51fc173ffc28bd6343f04ed2ec");
+  }
+
   render() {
     return (
       <Html lang="en">
         <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
+          />
           <link rel="shortcut icon" href="/favicon.png" />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <script
+            type="text/javascript"
+            src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"
+          ></script>
         </body>
       </Html>
     );
